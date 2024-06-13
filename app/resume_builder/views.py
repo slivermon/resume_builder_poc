70334fc95fbf300708681553f665af7f5cd0f714
slivermon <shawn.livermon@gmail.com>
2024-06-13 10:18:15 -0700

    fix: views filters timeline_event model for only the logged in user; migration files updated

--- app/resume_builder/views.py
+++ app/resume_builder/views.py
@@ -1,30 +1,23 @@
 # from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import HttpResponse, get_object_or_404, render
-from django.views.generic import (TemplateView)
-# from .models import Timeline_Event, Timeline_Event_Detail
+from django.views.generic import (TemplateView, ListView)
+from .models import Timeline_Event, Timeline_Event_Detail
 from .forms import TimelineForm
 # from accounts.models import CustomUser
 
-class Index(TemplateView):
-    template_name = "resume_builder/index.html"
-    # events = 1
-    # context = {
-    #     "timeline": events
-    # }
-    # render(request, "resume_builder/index.html", context)
+class Index(LoginRequiredMixin, ListView):
+    model = Timeline_Event
+    template_name = "resume_builder/index.html"    
 
-class Editor(TemplateView):
+    def get_queryset(self):
+        print(self.request.user.id)
+        return Timeline_Event.objects.filter(user_id=self.request.user.id)
+
+
+class Editor(LoginRequiredMixin, TemplateView):
     template_name = "resume_builder/editor.html"
 
-# @login_required
-# def editor(request):
-#     if request.method == "POST":
-#         return HttpResponse("POST to editor/")
-#     else:
-#         context = {}
-#         context["form"] = TimelineForm()
-#         return render(request, "resume_builder/editor.html", context)
 
 class Download(LoginRequiredMixin, TemplateView):
     template_name = "resume_builder/download.html"
