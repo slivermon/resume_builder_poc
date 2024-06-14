6a54aeefae08d276c6d5197c0e28eb8bdf34134d
slivermon <shawn.livermon@gmail.com>
2024-06-13 17:49:54 -0700

    feat: Added cols for fields in Django admin

--- app/resume_builder/admin.py
+++ app/resume_builder/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 # from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import Timeline_Event, Timeline_Event_Detail
 
-class UserTimelineAdmin(admin.ModelAdmin):
-    pass
+class TimelineEventAdmin(admin.ModelAdmin):
+    list_display = ("user_id", "timeline_start_date", "org_name", "role_name")
 
 
-admin.site.register(Timeline_Event)
-admin.site.register(Timeline_Event_Detail)
+class TimelineEventDetailAdmin(admin.ModelAdmin):
+    list_display = ("user_id", "timeline_event_id", "content")
+
+
+admin.site.register(Timeline_Event, TimelineEventAdmin)
+admin.site.register(Timeline_Event_Detail, TimelineEventDetailAdmin)
