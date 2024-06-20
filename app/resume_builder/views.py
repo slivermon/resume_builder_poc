91a502dd12a2b98b4d931c14ce6278dd4cb58b78
slivermon <shawn.livermon@gmail.com>
2024-06-19 18:53:12 -0700

    fix: index shows roles, companies, and detail in bulleted list

--- app/resume_builder/views.py
+++ app/resume_builder/views.py
@@ -12,18 +12,34 @@ class Index(LoginRequiredMixin, ListView):
     model = Timeline_Event
     template_name = "resume_builder/index.html"
 
-    # def get_queryset(self):
-    #     context = Timeline_Event.objects.filter(
+    # def get_queryset(self):     
+    #     return Timeline_Event.objects.filter(
     #         user_id=self.request.user.id,
     #         timeline_event_detail__user_id=self.request.user.id,
-    #     ).order_by("-timeline_start_date")
-    #     return context
+    #         ).order_by("-timeline_start_date")
 
-    def get_queryset(self):
-        context = Timeline_Event.objects.filter(
-            user_id=self.request.user.id,
-            timeline_event_detail__user_id=self.request.user.id,
-        ).order_by("-timeline_start_date")
+    # def get_queryset(self):     
+    #     return Timeline_Event_Detail.objects.select_related("timeline_event_id")
+
+    def get_queryset(self):     
+        queryset = Timeline_Event.objects.filter(user_id=self.request.user.id)
+        context = []
+        for item in queryset:
+            details = []
+            qs = Timeline_Event_Detail.objects.filter(timeline_event_id=item.pk)
+            for detail in qs:
+                details.append(
+                    detail.content
+                ) 
+            context.append({
+                "pk": item.pk,
+                "role": item.role_name,
+                "company": item.org_name,
+                "start_date": item.timeline_start_date,
+                "end_date": item.timeline_end_date,
+                "content": details,               
+            })
+                       
         return context
 
 
